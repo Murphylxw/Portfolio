4922129f9c5b463ee8535cbbe1d7a6d227add928
murphy <lorenz0faraway@gmail.com>
2025-10-01 15:47:46 +0200

    Parcurs finis

--- src/projets/pandora/script.js
+++ src/projets/pandora/script.js
@@ -8,3 +8,28 @@ menuBtn.addEventListener("click", () => {
 
 // script.js
 
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggles = document.querySelectorAll(".toggle-detail");
+
+  toggles.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const detail = btn.nextElementSibling;
+
+      if (detail.classList.contains("max-h-0")) {
+        detail.classList.remove("max-h-0");
+        detail.classList.add("max-h-40"); // ajuste la hauteur max selon ton contenu
+      } else {
+        detail.classList.add("max-h-0");
+        detail.classList.remove("max-h-40");
+      }
+    });
+  });
+});
+
+
