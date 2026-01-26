c1a69862e2d54916989f93848a66e0c1942f407e
Lorenzo FARRUGGIO <lorenzo.farruggio@experconnect.com>
2026-01-26 16:58:51 +0100

    jsp juste un commit avec la dernière version

---
+++ src/projets/multibureau/script.js
@@ -0,0 +1,32 @@
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
+// Carrousel des images
+document.addEventListener("DOMContentLoaded", () => {
+  const images = ["bureau1.png","bureau2.png"];
+  let currentIndex = 0;
+
+  const carouselImg = document.getElementById("carousel-img");
+  const prevBtn = document.getElementById("prev-btn");
+  const nextBtn = document.getElementById("next-btn");
+  const imageCounter = document.getElementById("image-counter");
+
+  function updateCarousel() {
+    carouselImg.src = images[currentIndex];
+    imageCounter.textContent = `${currentIndex + 1} / ${images.length}`;
+  }
+
+  prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateCarousel();
+  });
+
+  nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarousel();
+  });
+});
